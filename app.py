3cc1572b655be8a021f79ee5b7bbb64e64b0a6a1
doni <Doni23kj@gmail.com>
2026-05-13 09:26:51 +0600

    commit

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ class QLearningAgent:
             return np.random.randint(2)
         return np.argmax(self.q_table[state_key])
     
-    def learn(self, state, action, reward, next_state, done): # Функция обучения агента
+    def learn(self, state, action, reward, next_state, done):
         state_key = self.discretize(state)
         next_key = self.discretize(next_state)
         
@@ -147,7 +147,7 @@ class QLearningAgent:
         if not done:
             target += self.gamma * np.max(self.q_table[next_key])
             
-        self.q_table[state_key][action] += self.alpha * (target - self.q_table[state_key][action]) # Обновление Q-таблицы (сравнение)
+        self.q_table[state_key][action] += self.alpha * (target - self.q_table[state_key][action])
 
 def draw_game_frame(game):
     """Рисует HTML кадр игры"""
@@ -324,7 +324,7 @@ if st.session_state.running:
         while steps < 1000 and st.session_state.running:
             action = st.session_state.agent.get_action(state)
             next_state, reward, done = st.session_state.game.step(action)
-            st.session_state.agent.learn(state, action, reward, next_state, done) # обучения агента
+            st.session_state.agent.learn(state, action, reward, next_state, done)
             
             state = next_state
             episode_reward += reward
